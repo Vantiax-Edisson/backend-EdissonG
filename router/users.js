55474e57b766e36718dcd30bbfcf3372e89ffe39
EdissonG <edisson.guauta@gmail.com>
2024-10-15 20:12:23 -0500

    Ajustes de Create

--- router/users.js
+++ router/users.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import {allUser,userByRole}  from "../controllers/users/read.js";
+import {allUser,userByRole,userById}  from "../controllers/users/read.js";
+import { create } from "../controllers/users/create.js";
 
 const routerUsers = Router()
 
 routerUsers.get('/all', allUser)
 routerUsers.get('/role/:x',userByRole)
+routerUsers.get('/id/:id',userById)
+routerUsers.post('/create',create)
+routerUsers.post('createAll',)
 
 export default routerUsers
